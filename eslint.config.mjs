296be3d60eefac06ae4010ade7f9f129c8fea4b8
Scorpi-ON <s.scorpi-on@ya.ru>
2025-03-05 11:43:07 +0300

    fix: many improvements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,37 @@
 import { includeIgnoreFile } from "@eslint/compat";
-import eslint from "@eslint/js";
+import js from "@eslint/js";
 import path from "path";
 import tseslint from "typescript-eslint";
 
 const gitignorePath = path.resolve(import.meta.dirname, ".gitignore");
 
-const tsConfig = {
-    files: ["**/*.ts"],
-    languageOptions: {
-        parserOptions: {
-            project: true,
-            tsconfigRootDir: import.meta.dirname,
+export default tseslint.config(
+    includeIgnoreFile(gitignorePath),
+
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "commonjs",
+        },
+        extends: [js.configs.recommended],
+    },
+
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "commonjs",
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        extends: [js.configs.recommended, tseslint.configs.strictTypeChecked, tseslint.configs.stylisticTypeChecked],
+        rules: {
+            "@typescript-eslint/restrict-template-expressions": "off",
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "@typescript-eslint/no-extraneous-class": "off",
         },
     },
-    extends: [eslint.configs.recommended, tseslint.configs.strictTypeChecked, tseslint.configs.stylisticTypeChecked],
-    rules: {
-        "@typescript-eslint/restrict-template-expressions": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/no-extraneous-class": "off",
-    },
-};
-
-const jsConfig = {
-    files: ["**/*.js"],
-    languageOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    extends: [eslint.configs.recommended],
-};
-
-export default tseslint.config(includeIgnoreFile(gitignorePath), tsConfig, jsConfig);
+);
