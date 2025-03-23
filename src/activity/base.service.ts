2b933750ba37248c7b05213aa6bd18abe905b715
Scorpi-ON <s.scorpi-on@ya.ru>
2025-03-23 15:31:00 +0300

    fix(ts): fixed type error (in a bad way)

--- src/activity/base.service.ts
+++ src/activity/base.service.ts
@@ -19,9 +19,9 @@ import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import { eventTypes, events, locations, modules, responsiblePersons } from "../drizzle/drizzle.schema";
 import * as schema from "../drizzle/drizzle.schema";
 
-export abstract class BaseService<
-    T extends typeof eventTypes | typeof modules | typeof responsiblePersons | typeof locations,
-> {
+type BaseTable = typeof eventTypes | typeof modules | typeof responsiblePersons | typeof locations;
+
+export abstract class BaseService<T extends BaseTable> {
     protected static CURRENT_MONTH_EVENT_COUNT_SQL_EXPR = count(sql`CASE
         WHEN date_trunc('month', current_date) >= date_trunc('month', ${events.startDates}[1])
             AND ${events.endDate} IS NULL OR date_trunc('month', current_date) <= date_trunc('month', ${events.endDate})
@@ -38,7 +38,8 @@ export abstract class BaseService<
     async getIdByName(name: string) {
         const queryResults = await this.db
             .select({ id: this.table.id })
-            .from(this.table)
+            // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+            .from(this.table as BaseTable)
             .where(eq(this.table.name, name))
             .limit(1);
         if (queryResults.length > 0) {
@@ -54,21 +55,27 @@ export abstract class BaseService<
             currentMonthEventCount: BaseService.CURRENT_MONTH_EVENT_COUNT_SQL_EXPR,
         },
     ) {
-        return this.db
-            .select(select)
-            .from(this.table)
-            .leftJoin(events, eq(this.eventForeignKey, this.table.id))
-            .where(eq(this.table.isDeleted, false))
-            .groupBy(this.table.id)
-            .orderBy(desc(BaseService.CURRENT_MONTH_EVENT_COUNT_SQL_EXPR), asc(this.table.name));
+        return (
+            this.db
+                .select(select)
+                // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+                .from(this.table as BaseTable)
+                .leftJoin(events, eq(this.eventForeignKey, this.table.id))
+                .where(eq(this.table.isDeleted, false))
+                .groupBy(this.table.id)
+                .orderBy(desc(BaseService.CURRENT_MONTH_EVENT_COUNT_SQL_EXPR), asc(this.table.name))
+        );
     }
 
     async search(name?: string) {
-        return this.db
-            .select({ id: this.table.id, name: this.table.name })
-            .from(this.table)
-            .where(and(name ? ilike(this.table.name, `%${name}%`) : undefined, eq(this.table.isDeleted, false)))
-            .orderBy(asc(this.table.name));
+        return (
+            this.db
+                .select({ id: this.table.id, name: this.table.name })
+                // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+                .from(this.table as BaseTable)
+                .where(and(name ? ilike(this.table.name, `%${name}%`) : undefined, eq(this.table.isDeleted, false)))
+                .orderBy(asc(this.table.name))
+        );
     }
 
     async deleteOne(id: string) {
@@ -79,7 +86,7 @@ export abstract class BaseService<
         if (isUsedInEvent) {
             await this.db
                 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-                .update(this.table as typeof eventTypes | typeof modules | typeof responsiblePersons | typeof locations)
+                .update(this.table as BaseTable)
                 .set({ isDeleted: true })
                 .where(eq(this.table.id, id));
             return { deleted: false, restored: true };
